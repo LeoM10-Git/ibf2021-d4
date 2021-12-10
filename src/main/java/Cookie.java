c026bfbe41c09617a11686dac20b5de5cb6203aa
LeoM10-Git <cc20082012@yahoo.com.sg>
2021-12-10 11:33:43 +0800

    add code in Cookie.java, to include text file in jar file

--- src/main/java/Cookie.java
+++ src/main/java/Cookie.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -11,22 +12,17 @@ public class Cookie {
 
 
     public ArrayList<String> generateCookie() throws IOException {
-        Path path = Paths.get("src/cookie_file.txt");
-        File file = path.toFile(); //convert to file from path
+        InputStream is = Cookie.class.getClassLoader().getResourceAsStream("cookie_file.txt");
+//        Path path = Paths.get("src/main/resources/cookie_file.txt");
+//        File file = path.toFile(); //convert to file from path
 
         ArrayList<String> cookie_text = new ArrayList<>();
 
-        Scanner sc = null;
-        try {
-            sc = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        Scanner sc = new Scanner(is);
         while (sc.hasNext()){
             cookie_text.add(sc.nextLine());
         }
         sc.close();
         return cookie_text;
     }
-
 }
